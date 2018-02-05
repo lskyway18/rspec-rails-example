99d0c6aecd065657ff5aad99f2ec9994138147ec
Lukas <lhorvat18@zoho.com>
2018-02-05 10:59:32 +0100

    trimmed not checked tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,16 +14,16 @@ describe User do
   end
 
   context '#issue_access_token' do
-    # it 'creates access token belonging to user' do
-    #   user = create(:user)
-    #
-    #   expect do
-    #     access_token = user.issue_access_token
-    #     expect(access_token).to be_persisted
-    #     expect(access_token.user).to eq user
-    #     expect(user.access_tokens).to eq [access_token]
-    #   end.to change { AccessToken.count }.by(1)
-    # end
+    it 'creates access token belonging to user' do
+      user = create(:user)
+
+      expect do
+        access_token = user.issue_access_token
+        expect(access_token).to be_persisted
+        expect(access_token.user).to eq user
+        expect(user.access_tokens).to eq [access_token]
+      end.to change { AccessToken.count }.by(1)
+    end
   end
 
 end
--- spec/request/SEE_API_SPECS.md
+++
@@ -1,1 +0,0 @@
-For request specs, see API specs in [`../api/v1/`](../api/v1).
